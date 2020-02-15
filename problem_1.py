b75a9a2e11dea25a75cb8a9a828a58c76ed10b4a
Sma <Ibrahim_Sma@Dell.com>
2020-02-15 14:32:25 +0530

    Added Author and contact info

--- problem_1.py
+++ problem_1.py
@@ -1,3 +1,9 @@
+# @author : Ibrahim Sma
+# @github_location: https://github.com/ibrahim-sma
+# @email: ibrahim.sma1990@gmail.com
+# @LinkedIn: www.linkedin.com/in/ibrahim-sma
+
+
 ## Problem 1 -  Multiples of 3 and 5
 
 ##If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -15,8 +21,11 @@ print(f'The sum of all the multiples of 3 or 5 below 1000 is : {total}')
 ## Solution guidelines:
 
 ##1) Assign initial total as 0.
-##2) Loop over the range from 1 to 999
-##3) if any number is divisible by 3 or 5
-##4) Add that number to the total
-##5) Finally, print the total number
 
+##2) Loop over the range from 1 to 999
+
+##3) if any number is divisible by 3 or 5
+
+##4) Add that number to the total
+
+##5) Finally, print the total number
